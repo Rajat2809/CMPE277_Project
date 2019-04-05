ee6b59a01630114327516ea6fbffcd26a18beb0b
Rajat2809 <36286079+Rajat2809@users.noreply.github.com>
2019-04-04 17:25:29 -0700

    Done Changes for complete Login functionality with FOrgot password and email verification

--- java/com/example/fitnessapp/MainActivity.java
+++ java/com/example/fitnessapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fitnessapp;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +14,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fitnessapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private EditText email,pass;
     private Button btLogin;
     private TextView tvSignUp;
+    private TextView tvForgotPassword;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
    // DBHandler dbHandler;
 
     @Override
@@ -25,47 +35,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         validate();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user !=null) {
+            finish();
+            Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+            Intent dataIntent = new Intent(MainActivity.this, Home.class);
+            dataIntent.putExtra("email", email.getText().toString().trim());
+            startActivity(dataIntent);
+        }
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userEmail = email.getText().toString().trim();
-                String userPass = pass.getText().toString().trim();
-               // dbHandler =new DBHandler(MainActivity.this,null,null,1);
-                String dbEmail = "";
-                //dbEmail = dbHandler.databaseTOString(userEmail,userPass);
 
-                if(userEmail.isEmpty() || userPass.isEmpty()){
-                    // Toast.makeText(MainActivity.this, "Enter all details", Toast.LENGTH_SHORT).show();
-                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                    alertDialog.setTitle("Alert");
-                    alertDialog.setMessage("Enter all details");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-//                else if(dbEmail.isEmpty()){
-//                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-//                    alertDialog.setTitle("Alert");
-//                    alertDialog.setMessage("UserName or Password does not matches with records.");
-//                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-//                            new DialogInterface.OnClickListener() {
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    dialog.dismiss();
-//                                }
-//                            });
-//                    alertDialog.show();
-//                }
-                   else{
-                    Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
-                    Intent dataIntent = new Intent(MainActivity.this,Home.class);
-                    dataIntent.putExtra("email",userEmail);
-                    startActivity(dataIntent);
+                validateUser(email.getText().toString().trim(),pass.getText().toString().trim());
 
-                }
+
+
             }
         });
         tvSignUp.setOnClickListener(new View.OnClickListener() {
@@ -75,12 +63,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PasswordReset.class);
+                if(!email.getText().toString().trim().isEmpty()) {
+                    intent.putExtra("email", email.getText().toString().trim());
+                }
+                startActivity(intent);
+            }
+        });
+
     }
     private void validate(){
         email = (EditText)findViewById(R.id.etEmail);
         pass = (EditText)findViewById(R.id.etPass);
         btLogin = (Button)findViewById(R.id.btnLogin);
         tvSignUp = (TextView)findViewById(R.id.tvSignUP);
+        tvForgotPassword =(TextView)findViewById(R.id.tvForgotPassword);
+    }
+
+    private void validateUser(final String email, String password){
+
+        progressDialog.setMessage("You are about to enter the world of fitness.");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent dataIntent = new Intent(MainActivity.this, Home.class);
+                    dataIntent.putExtra("email", email);
+                    startActivity(dataIntent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
     }
 
 }
--- java/com/example/fitnessapp/SignUp.java
+++ java/com/example/fitnessapp/SignUp.java
@@ -1,6 +1,7 @@
 package com.example.fitnessapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +10,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.fitnessapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
     private EditText FullName,email,pass;
     private Button btnSignUp;
     private TextView tvLogin;
+   private FirebaseAuth firebaseAuth;
+
+
   //  DBHandler dbHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +37,28 @@ public class SignUp extends AppCompatActivity {
                 String fuName = FullName.getText().toString().trim();
                 String userEmail = email.getText().toString().trim();
                 String userPass = pass.getText().toString().trim();
-//                dbHandler =new DBHandler(SignUp.this,null,null,1);
-//                Data_Signup signup = new Data_Signup(fuName,userEmail,userPass);
-//                dbHandler.addSignUp(signup);
+                Boolean valid=false;
                 if(fuName.isEmpty() || userEmail.isEmpty() || userPass.isEmpty()){
                     Toast.makeText(SignUp.this, "Fill Up all details.", Toast.LENGTH_SHORT).show();
                 }else{
-                    startActivity(new Intent(SignUp.this, com.example.fitnessapp.MainActivity.class));
+                    firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.createUserWithEmailAndPassword(userEmail,userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                sendUserData();
+                                Toast.makeText(SignUp.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUp.this, com.example.fitnessapp.MainActivity.class));
+                            }else{
+                                Toast.makeText(SignUp.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
                 }
+
+
+
 
 
             }
@@ -51,10 +74,19 @@ public class SignUp extends AppCompatActivity {
 
     }
     private void validate(){
-        FullName = (EditText)findViewById(R.id.etFullName);
+        FullName = (EditText)findViewById(R.id.etFullProfileName);
         email = (EditText)findViewById(R.id.etUserEmail);
         pass = (EditText)findViewById(R.id.etPass);
-        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        btnSignUp = (Button)findViewById(R.id.btnUpdate);
         tvLogin = (TextView)findViewById(R.id.tvLogin);
+    }
+    private void sendUserData(){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        User user = new User(email.getText().toString().trim(),pass.getText().toString().trim(),FullName.getText().toString().trim());
+        if(databaseReference.setValue(user).isSuccessful()){
+            firebaseAuth.signOut();
+            Toast.makeText(this, "Uploading User Data Successful", Toast.LENGTH_SHORT).show();
+        }
     }
 }
